d03ebcc51f27521a18e046dc3189e4d4b12e7f96
Julien Guerinet <julien@guerinet.com>
2014-05-01 14:15:01 -0400

    Updated the URL

--- src/stringparser/StringParser.java
+++ src/stringparser/StringParser.java
@@ -17,7 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringParser{
-    public static final String URL = "PUT_URL_HERE";
+
+    public static final String URL = "https://docs.google.com/spreadsheet/pub?key=0AhHWqylMWqY4dGZFcEZQYThKV1ZjRVhIQ3dHOWNsOFE&single=true&gid=1&output=csv";
 
     public static final String ANDROID_ENGLISH_STRINGS_PATH = "PUT_ROOT_HERE/res/values/strings.xml";
     public static final String ANDROID_FRENCH_STRINGS_PATH = "PUT_ROOT_HERE/res/values-fr/strings.xml";
