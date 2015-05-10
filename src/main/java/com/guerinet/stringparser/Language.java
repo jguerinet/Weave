938c64a540fe6f15ceaa4386d6b5fdf8d7c5f513
Julien Guerinet <julien@guerinet.com>
2015-05-10 14:20:12 -0400

    Moving the app to Gradle.

---
+++ src/main/java/com/guerinet/stringparser/Language.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013-2015 Julien Guerinet.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.guerinet.stringparser;
+
+/**
+ * One language that the Strings are in
+ * @author Julien Guerinet
+ * @version 2.4
+ * @since 2.0
+ */
+public class Language {
+    /**
+     * The language Id
+     */
+    private String id;
+    /**
+     * The language path
+     */
+    private String path;
+    /**
+     * The column index of the language in the CSV file
+     */
+    private int columnIndex;
+
+    /**
+     * Default Constructor
+     *
+     * @param id   The language Id
+     * @param path The language path
+     */
+    public Language(String id, String path){
+        this.id = id;
+        this.path = path;
+        this.columnIndex = -1;
+    }
+
+    /* GETTERS */
+
+    /**
+     * @return The language Id
+     */
+    public String getId(){
+        return this.id;
+    }
+
+    /**
+     * @return The language path
+     */
+    public String getPath(){
+        return this.path;
+    }
+
+    /**
+     * @return The column index of this language
+     */
+    public int getColumnIndex(){
+        return this.columnIndex;
+    }
+
+    /* SETTERS */
+
+    /**
+     * @param columnIndex The column index of this language
+     */
+    public void setColumnIndex(int columnIndex){
+        this.columnIndex = columnIndex;
+    }
+}
---
+++ src/main/java/com/guerinet/stringparser/LanguageString.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2013-2015 Julien Guerinet.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.guerinet.stringparser;
+
+import java.util.HashMap;
+
+/**
+ * One String with all of the translations
+ * @author Julien Guerinet
+ * @version 2.4
+ * @since 1.0
+ */
+public class LanguageString {
+    /**
+     * The key to store the String under
+     */
+    private String key;
+    /**
+     * A HashMap of translations, with the key being the language Id and the value being the String
+     */
+    private HashMap<String, String> translations;
+
+    /**
+     * Default Constructor
+     *
+     * @param key The String key
+     */
+    public LanguageString(String key){
+        this.key = key;
+        this.translations = new HashMap<String, String>();
+    }
+
+    /* GETTERS */
+
+    /**
+     * @return The String Key
+     */
+    public String getKey(){
+        return this.key;
+    }
+
+    /**
+     * Get the String in a given language
+     *
+     * @param id The language Id
+     * @return The String in that language
+     */
+    public String getString(String id){
+        return this.translations.get(id);
+    }
+
+    /* SETTERS */
+
+    /**
+     * Add a translation
+     *
+     * @param id     The language Id
+     * @param string The String
+     */
+    public void addTranslation(String id, String string){
+        this.translations.put(id, string);
+    }
+}
