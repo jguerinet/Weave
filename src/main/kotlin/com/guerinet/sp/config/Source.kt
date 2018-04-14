68f8c4e8242e27ebd15b45a0b9121d4d0c034b88
Julien Guerinet <julien@guerinet.com>
2018-04-14 18:24:44 -0400

    Create Config related models to parse config Json

---
+++ src/main/kotlin/com/guerinet/sp/config/Source.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2013-2018 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.guerinet.sp.config
+
+/**
+ * One source from where the Strings could be coming from
+ * @author Julien Guerinet
+ * @since 5.0.0
+ *
+ * @param title Name of this source (for logging purposes)
+ * @param url   Url to retrieve the Strings from
+ */
+class Source(val title: String, val url: String)
