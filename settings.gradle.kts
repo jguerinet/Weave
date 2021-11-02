6526329a2cd6a6c32e11dc5fe2dc9b29be96beba
jguerinet <julien@guerinet.com>
2021-11-02 09:05:07 -0400

    Kotlinize Settings file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2019 Julien Guerinet
+ * Copyright 2013-2021 Julien Guerinet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,4 +15,4 @@
  *
  */
 
-rootProject.name = 'weave'
+rootProject.name = "weave"
